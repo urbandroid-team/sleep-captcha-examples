65c8d94d4746aebc059e47bccc581c53258e5321
Artaud <richtej2@gmail.com>
2015-12-29 16:09:20 +0100

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# sleep-captcha-examples
-Sleep Captcha Examples
+# Sleep as Android - Captcha Examples
+
+[Sleep as Android](https://play.google.com/store/apps/details?id=com.urbandroid.sleep) is an Android app that helps you sleep better. To help you with waking up, Sleep as Android offers you tasks to solve when the alarm rings. The alarm will stop ringing once you solve the task. These tasks are called **CAPTCHAs**.
+
+Sleep as Android has an open API for CAPTCHAs, so you can create your own CAPTCHAs to solve in the morning. This repository gives out some examples. Feel free to build upon it!
+
+## API overview
+
+To get an overview of the CAPTCHA API and the Captcha lifecycle, see [the docs on our website](http://sleep.urbandroid.org/documentation/developer-api/captcha-api/).
+
+## Example documentation
+
+There is one CAPTCHA in the example project at the moment - ReverseCaptcha.
+
+### ReverseCaptcha
+
+The heart of the CAPTCHA is the CaptchaActivity, in the example this is `ReverseCaptchaActivity.java`. This activity shows up when the Captcha is called by the alarm.
+
+
+
+
+### TODO Na zaver:
+In Sleep as Android, you choose the active CAPTCHA in **Settings > CAPTCHA**. Once you build a new CAPTCHA apk and install it, your CAPTCHA will also appear there.
+
+* Captcha-support library
+* [CAPTCHA 2016 Hackathon](http://sleep.urbandroid.org/captcha-hackathon-12016/)
--- app/src/main/java/com/urbandroid/sleep/captcha/example/ReverseCaptchaActivity.java
+++ app/src/main/java/com/urbandroid/sleep/captcha/example/ReverseCaptchaActivity.java
@@ -2,46 +2,92 @@ package com.urbandroid.sleep.captcha.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.urbandroid.slee.captcha.example.R;
 import com.urbandroid.sleep.captcha.CaptchaSupport;
 import com.urbandroid.sleep.captcha.CaptchaSupportFactory;
+import com.urbandroid.sleep.captcha.RemainingTimeListener;
 
+import java.util.Random;
+
+// this is the main captcha activity
 public class ReverseCaptchaActivity extends Activity {
 
-    private CaptchaSupport captchaSupport;
+    private CaptchaSupport captchaSupport; // include this in every captcha
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        captchaSupport = CaptchaSupportFactory.create(this);
+        captchaSupport = CaptchaSupportFactory.create(this); // include this in every captcha, in onCreate()
 
+        // show timeout in TextView with id "timeout"
+        final TextView timeoutView = (TextView) findViewById(R.id.timeout);
+        captchaSupport.setRemainingTimeListener(new RemainingTimeListener() {
+            @Override
+            public void timeRemain(int seconds, int aliveTimeout) {
+                timeoutView.setText(seconds + "/" + aliveTimeout);
+            }
+        });
+
+        // show difficulty in TextView with id "difficulty", read from captchaSupport.getDifficulty()
         final TextView difficultyView = (TextView) findViewById(R.id.difficulty);
         difficultyView.setText(getResources().getString(R.string.difficulty, captchaSupport.getDifficulty()));
 
+        // show a random string of length = (difficulty * 3) in TextView with id "captcha_text"
+        final TextView captchaTextView = (TextView) findViewById(R.id.captcha_text);
+        final String captchaText = randomString(captchaSupport.getDifficulty() * 3);
+        captchaTextView.setText(captchaText);
+
+
+        final EditText input_text = (EditText) findViewById(R.id.input_text);
+
+        // send captchaSupport.solved() when correct string entered and "Done" button tapped
         findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                captchaSupport.solved();
-                finish();
+                Log.i("ReverseCaptchaText", captchaText);
+                Log.i("ReverseCaptchaInput", input_text.getText().toString());
+                if (input_text.getText().toString().equals(new StringBuilder(captchaText).reverse().toString())) {
+                    captchaSupport.solved(); // .solved() broadcasts an intent back to Sleep as Android to let it know that captcha is solved
+                    finish();
+                }
             }
         });
 
     }
 
+    public static String randomString(int length) {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        Random generator = new Random();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < length; i++ ) {
+            char c = chars[generator.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final EditText input_text = (EditText) findViewById(R.id.input_text);
+        input_text.requestFocus();
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        captchaSupport.unsolved();
+        captchaSupport.unsolved(); // .unsolved() broadcasts an intent back to AlarmAlertFullScreen that captcha was not solved
     }
 
     @Override
     public void onUserInteraction() {
         super.onUserInteraction();
-        captchaSupport.alive();
+        captchaSupport.alive(); // .alive() refreshes captcha timeout - intended to be sent on user interaction primarily, but can be called anytime anywhere
     }
 }
